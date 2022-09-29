36e8794e8d61f2d9e5efee2fe3aaf81af12e2ba7
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 16:32:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "a507f13d-99f7-45c0-9f34-7e0c94df0cc2",
+    "home_feed": [
+        {
+            "id": "ffbdf1f0-641c-4ef0-bac3-3da94e4c9ac2",
+            "artist_name": "black",
+            "home_carousel": [
+                {
+                    "id": "25901930-1567-4eec-9359-ff4cf2c5af7e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                },
+                {
+                    "id": "c4a00719-a658-439b-8259-a72e69694691",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+                },
+                {
+                    "id": "13273687-6edc-4a53-b495-ae9dfc860d1b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "afc15ce7-c6dc-411f-b22c-8fc8fa97ef8f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "d5e28d3d-77ad-4c61-afdc-a560cb3369c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                }
+            ],
+            "art_caption": "pattern",
+            "art_description": "Less eat three avoid. Throughout newspaper trouble add maybe ever health animal. Improve change find but director. Health car open doctor."
+        },
+        {
+            "id": "030b4812-ffd5-4374-bc4c-7088b70aa2cd",
+            "artist_name": "fight",
+            "home_carousel": [
+                {
+                    "id": "0381de91-8b9d-489f-a566-73532ecac843",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "276a75de-49e4-4fb2-83ec-06fa6db0c120",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "d9e4b044-f3a0-471b-a092-fead3af23c7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "00a5cfa2-8464-4e71-b8c0-02e05e0f49cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                },
+                {
+                    "id": "e250fd4e-53ce-4e2c-828d-3501ced168d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                }
+            ],
+            "art_caption": "painting",
+            "art_description": "Have including Republican night according eat. Agency attack evidence single. Focus indicate condition."
+        },
+        {
+            "id": "e1f7c344-2434-4a82-b97f-a1b0909f194e",
+            "artist_name": "society",
+            "home_carousel": [
+                {
+                    "id": "db8cdcdf-d428-4585-8faa-33dab7925320",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "b81af7a6-de12-4eef-a2ef-6519c056d7b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "d15d205f-14c6-4cd1-be6e-4d503b5889c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/224.png"
+                },
+                {
+                    "id": "8061f0dd-112f-4b39-a4bd-b9a53cc6edb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "f3041160-e9cc-449e-9a98-7cc8504ad7db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                }
+            ],
+            "art_caption": "beautiful",
+            "art_description": "End center hold concern kind rise. Student teacher threat service try together. Go minute be."
+        },
+        {
+            "id": "e3b77a48-7e46-4bf1-880a-450466d91dc3",
+            "artist_name": "career",
+            "home_carousel": [
+                {
+                    "id": "178b35df-d4b4-4553-a62b-125f705ed691",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "21a2fa76-e521-4ed5-93b3-0a3095236635",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "ca82db25-608e-4a8e-8496-1ac0efdfffac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png"
+                },
+                {
+                    "id": "57bb014e-c6b8-4cd4-ba0e-07bb511c810b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png"
+                },
+                {
+                    "id": "0e2e398c-12e2-4950-a330-b55065ed18f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                }
+            ],
+            "art_caption": "subject",
+            "art_description": "Security election daughter. Land tree everything benefit concern. Easy consider evidence try hundred of."
+        },
+        {
+            "id": "4f0fa332-29f7-452f-958e-70e9472e94d0",
+            "artist_name": "produce",
+            "home_carousel": [
+                {
+                    "id": "d0949089-cbad-4ab5-b0de-decc194f4277",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "3b6135c6-a04a-4916-839c-84839ea63097",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "fd54026d-2872-4af3-8ea0-841ce07f274a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "ed438686-acdd-43a9-be4c-fb1c5e543b0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png"
+                },
+                {
+                    "id": "a198dd7a-540e-4889-96b8-cf77b4b8a131",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                }
+            ],
+            "art_caption": "school",
+            "art_description": "Although anyone check official life while. Above author try court design world."
+        }
+    ]
+}
